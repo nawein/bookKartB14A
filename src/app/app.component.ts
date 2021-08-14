db8582d4a6d91a43585a028d9381b5307dabde4e
nawein <naveendn26@gmmail.com>
2021-08-14 16:05:41 +0530

    themes Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'bookKartB14A';
+export class AppComponent implements OnInit {
+  title = 'angular-material-theming-demo';
+  theme = 'DeepPurple-light-theme';
+
+  constructor(private overlayContainer: OverlayContainer) {}
+
+  public ngOnInit(): void {
+    this.overlayContainer.getContainerElement().classList.add(this.theme);
+  }
+
+  public onThemeChange(): void {
+    const overlayContainerClasses =
+      this.overlayContainer.getContainerElement().classList;
+    console.log(overlayContainerClasses);
+
+    const themeClassesToRemove = Array.from(overlayContainerClasses).filter(
+      (item: string) => item.includes('-theme')
+    );
+    console.log(themeClassesToRemove);
+    console.log(themeClassesToRemove.length);
+
+    if (themeClassesToRemove.length) {
+      overlayContainerClasses.remove(...themeClassesToRemove);
+    }
+    overlayContainerClasses.add(this.theme);
+    console.log(this.theme);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material Components
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
